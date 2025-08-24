0e8273abca2ceee24043ad07935b3928239a7d41
maungminmin <68054935+maungminmin@users.noreply.github.com>
2025-08-24 14:35:29 +0630

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-const userID = Deno.env.get('UUID') || 'd342d11e-d424-4583-b36e-524ab1f0afa4'
+const userID = Deno.env.get('UUID') || '016453f5-7f5c-4022-99dd-b8874c6ac5da'
 const proxyIP = Deno.env.get('PROXYIP') || ''
 
 if (!isValidUUID(userID)) {
